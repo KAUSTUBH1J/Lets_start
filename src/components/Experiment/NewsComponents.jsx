2dfc77e7609f558f54f1cc3e722fef24ab84b521
Kaustubh1J <kaustubh1j@gmail.com>
2024-10-10 23:15:02 +0530

    new class components

--- src/components/Experiment/NewsComponents.jsx
+++ src/components/Experiment/NewsComponents.jsx
@@ -5,7 +5,14 @@ export default class NewsComponents extends Component{
     render(){
         return(
             <>
-                
+                <div className='container'>
+                    <h1>
+                        Today New's 
+                    </h1>
+                    <div style={{display: 'flex'}}>
+                        <Card title="abs"/>
+                    </div>
+                </div>
             </>
         )
     }
